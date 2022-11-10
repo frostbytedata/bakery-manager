fa92502e202505b857e7e73015b1408c8fee917b
frostbytedata <info@frostbytedata.com>
2022-11-10 00:38:18 -0500

    Adding base ui components module.

---
+++ app/src/app/components/ui/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'bm-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  @Output() bmClick = new EventEmitter<Event>();
+  @Input() icon: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

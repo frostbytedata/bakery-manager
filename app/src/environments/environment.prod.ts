41222ebf36073aeaed625911b6142f2a44bd506c
frostbytedata <info@frostbytedata.com>
2023-05-20 01:04:26 -0400

    Remove trailing slash at the end of api url

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://bakerymanager.us-east-1.elasticbeanstalk.com/',
+  apiUrl: 'http://bakerymanager.us-east-1.elasticbeanstalk.com',
 };
